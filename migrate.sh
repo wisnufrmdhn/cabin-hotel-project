089c9f380800f93ab5acb65e451f609fe245bc76
ilhaam43 <ilhaam43@gmail.com>
2023-10-16 08:25:56 +0700

    add: deployment for migrate bash command

--- migrate.sh
+++ migrate.sh
@@ -4,7 +4,14 @@
 if [ "$1" == "fresh" ]; then
     # Execute the migrate:fresh --seed command
     docker exec -it laravel-app php artisan migrate:fresh --seed
+elif [ "$1" == "migrate" ]; then
+    # Execute the migrate:fresh --seed command
+    docker exec -it laravel-app php artisan migrate:fresh --seed
+elif [ "$1" == "clear" ]; then
+    # Execute the clear commands
+    docker exec -it $container_name php artisan config:clear
+    docker exec -it $container_name php artisan cache:clear
 else
-    # Execute the migrate:seed command
-    docker exec -it laravel-app php artisan migrate:seed
+    echo "Invalid command. Usage: $0 [migrate|migrate:fresh|clear]"
+    exit 1
 fi
