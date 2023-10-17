7223c9fcdd5c98ee616844010251077dd6b8e9f7
ilhaam43 <ilhaam43@gmail.com>
2023-10-18 01:44:13 +0700

    fix: migrate bash script

--- migrate.sh
+++ migrate.sh
@@ -6,9 +6,10 @@ if [ "$1" == "fresh" ]; then
     docker exec -it laravel-app php artisan migrate:fresh --seed
 elif [ "$1" == "migrate" ]; then
     # Execute the migrate:fresh --seed command
-    docker exec -it laravel-app php artisan migrate:fresh --seed
+    docker exec -it laravel-app php artisan migrate --seed
 elif [ "$1" == "clear" ]; then
     # Execute the clear commands
+    docker exec -it $container_name php artisan route:clear
     docker exec -it $container_name php artisan config:clear
     docker exec -it $container_name php artisan cache:clear
 else
