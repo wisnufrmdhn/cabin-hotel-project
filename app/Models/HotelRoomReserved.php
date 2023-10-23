a2a0079e291ab031c3a05140735a809f7947be55
ilhaam43 <ilhaam43@gmail.com>
2023-10-23 16:49:41 +0700

    fix: forgot to add relation in hotelroomnumber

--- app/Models/HotelRoomReserved.php
+++ app/Models/HotelRoomReserved.php
@@ -22,6 +22,6 @@ class HotelRoomReserved extends Model
 
     public function hotelRoomNumber()
     {
-        return $this->belongsTo(hotelRoomNumber::class);
+        return $this->belongsTo(HotelRoomNumber::class);
     }
 }
